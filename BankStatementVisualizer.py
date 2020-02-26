ae69c72a46b8030299de7c7e78538e8012c6b192
Jonathan <jono-rogers@hotmail.com>
2020-02-26 19:56:25 +1300

    add comments

--- BankStatementVisualizer.py
+++ BankStatementVisualizer.py
@@ -7,9 +7,14 @@ import numpy as np
 #get bank statement data from CSV file
 #Get the value of the first array item:  x = cars[0]
 #Modify the value of the first array item: cars[0] = "Toyota"
+#use google API Places to find a suitable location then store decription in a database. 
+#database of the store name and the catagory. so we dont have to keep searching for it on google.
+#when printing show a graph for month to month / show line graph of out the normal and suggest the break like tlm
 
+
+#catagory common words
 income = ["Salaries"]
-eatingOut =["KFC", "Fish", "Lady","Malaysia","Britoma","Indian", "MCDONALDS", "Subway"]
+eatingOut =["KFC", "Fish", "Lady","Malaysia","Britomart","Indian", "MCDONALDS", "Subway"]
 shopping =[]
 entertainment =["KFC", "Lula", "asb", "Cassette", "Liquor", "Convenience", "Rooftop"]
 travel =[]
@@ -21,6 +26,7 @@ education =[]
 saving =["Holiday", "Education","funds", "Growth investments", "Education"]
 utilities =["Spark","Laundromat"]
 
+#setting the total amount to 0 for 
 catagories = {
   "total Income": 0,
   "Eating Out": 0,
@@ -39,53 +45,82 @@ catagories = {
 
 def is_in_catagory(amount,other_party):
 	print(type(amount))
+	listLength = len(other_party)
+	count = 0
 	for key_word in other_party:
 		print("teasting= key_word+" + str(key_word) +", amount="+ str(amount)+ ", other_p= " +str(key_word))
 		if amount == "Amount":
 			print("this was passed")
+			count +=1
+			break
 		elif key_word in eatingOut:
 			catagories["Eating Out"] += float(amount)
 			print("catagories Eating Out added + " + str(amount))
+			count +=1
+			break
 		elif key_word in entertainment:
 			catagories["Shopping"] += float(amount)
 			print("catagories Shopping added + " + str(amount))
+			count +=1
+			break
 		elif key_word in groceries:
 			catagories["Groceries"] += float(amount)
 			print("catagories Groceries added + " + str(amount))
+			count +=1
+			break
 		elif key_word in rent:
 			catagories["Rent"] += float(amount)
 			print("catagories rent added + " + str(amount))
+			count +=1
+			break
 		elif key_word in transport:
 			catagories["Transport"] += float(amount)
 			print("catagories Transport added + " + str(amount))
+			count +=1
+			break
 		elif key_word in saving:
 			catagories["Saving"] += float(amount)
 			print("catagories Saving added + " + str(amount))
+			count +=1
+			break
 		elif key_word in utilities:
 			catagories["Utilities"] += float(amount)
 			print("catagories Utilities added + " + str(amount))
+			count +=1
+			break
 		elif key_word in income:
 			catagories["total Income"] += float(amount)
 			print("catagories total Income added + " + str(amount))
+			count +=1
+			break
 		else:
-			catagories["Unallocated"] +=  float(amount)
-			print("catagories Unallocated added + " + amount)
+			if count != listLength:
+				print("pass")
+				count +=1
+				pass
+			else:
+				catagories["Unallocated"] +=  float(amount)
+				print("catagories Unallocated added + " + amount)
 
 
+#extract inforamtion from CSV file 
 with open('testJan20.csv', newline='') as bankStatement:
 	reader = csv.reader(bankStatement)
 	for row in reader:
+		#us function to allocate transaction into the right bucket
 		is_in_catagory(row[1],row[2].split())
 		#print(row[2].split())
 
 for x in catagories:
     print ("catagories = " +str(x)+str(catagories[x]))
 
+
 #exp_val = list(abs(catagories.values()))
 #exp_key = list(catagories.keys())
 exp_val = []
 exp_key = []
 k, v = [], []
+#split dectionary of category into two list so we can use the matplotlib
 for key, value in catagories.items():
 	if  key == "Unallocated":
 		print("skipped unallocated")
---
+++ README.md
@@ -0,0 +1,33 @@
+# Bank statement visualiser
+
+This project is a simple program used for people to run against their bank statements(in the form of a CSV.file). It will visually represent a pie graph breaking down the expense in respective categories
+
+## Reason for creation
+
++ I wanted to make something that would hep track my expense as I was losing track of my spending
+
+
+#### Setting Up?
+1. Python needs to be installed
+  + ` https://www.python.org/downloads/`
+
+2. import matplotlib on pc.
+  + `python -m pip install -U pip`
+  + `python -m pip install -U matplotlib`
+
+3.  export your bank statement as a CSV file with your respected bank.
+  + `change the open file to the name of your CSV file that you are running`
+  + `the columns might be different so you might have to keep that in mind and make adjustment prior to running`
+
+#### Going forward with Improvement
+
+1. I would like to make changes to the way the transaction description find a category. Looking for a API or a way that I can use google to look up stores to correctly categories the transaction
+
+2. Ill be making changes to the way the pie graph shows the data and any transaction not in a category. Ill be listing them for the user to decide the best allocation.
+
+3. any recommendation for the way i can improve the algorithm would be apricated.
+
+#### Prepare to deploy your project.
+
+###### Author
+Jonathan Rogers
